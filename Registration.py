38ac73fef3c98134e17d4d74975d189654807c73
lj5619 <leon.olaprath@gmail.com>
2025-03-03 15:53:53 +0530

    [Leon] Add: Python script to input password of minimum 8 characters

--- Registration.py
+++ Registration.py
@@ -7,13 +7,14 @@ def validate_first_name(input_string):
         Parameters:
             input string
         Return:
-            None
+            Boolean Value
     """
     pattern = r'^[A-Z][a-z]{2,}$'
     if re.fullmatch(pattern,input_string):
-        print('Valid First Name')
+        return True
     else:
         print('Invalid First Name')
+        return False
 
 def validate_last_name(input_string):
     """
@@ -22,14 +23,14 @@ def validate_last_name(input_string):
         Parameters:
             input string
         Return:
-            None
+            Boolean Value
     """
     pattern = r'^[A-Z][a-z]{2,}$'
     if re.fullmatch(pattern,input_string):
-        print('Valid Last Name')
+        return True
     else:
         print('Invalid Last Name')
-
+        return False
 def validate_email(email):
     """
         Description:
@@ -37,14 +38,15 @@ def validate_email(email):
         Parameters:
             email
         Return:
-            None
+            Boolean Value
     """
 
     pattern = r'^[A-Za-z0-9._%+-]+@[A-Za-z.]+\.[A-Za-z]{2,}$'
     if re.fullmatch(pattern,email):
-        print('Valid email address')
+        return True
     else:
         print('Invalid email address')
+        return False
 
 def validate_mobile(mobile_number):
     """
@@ -53,14 +55,33 @@ def validate_mobile(mobile_number):
         Parameters:
             mobile number
         Return:
-            None
+            Boolean Value
     """
 
     pattern = r'^[0-9]{2,} [0-9]{10}$'
     if re.fullmatch(pattern,mobile_number):
-        print('Valid mobile number')
+        return True
     else:
         print('Invalid mobile number')
+        return False
+    
+def check_password(password):
+    """
+        Description:
+            Funtion to check if the password matches the rules
+        Parameters:
+            password
+        Return:
+            Boolean value
+    """
+
+    pattern = r'.{8,}$'
+    if re.fullmatch(pattern,password):
+        print('Password is Valid')
+        return True
+    else:
+        print('Invalid Password')
+        return False
 
 def main():
 
@@ -68,12 +89,12 @@ def main():
     last_name = input('Enter Last Name: ')
     email = input('Enter Email address: ')
     mobile_number = input('Enter mobile number with country code (Eg: 91 9812345123): ')
-    validate_first_name(first_name) 
-    validate_last_name(last_name)
-    validate_email(email)
-    validate_mobile(mobile_number)
+    password = input('Enter Password (minimum 8 characters): ')
+    if validate_first_name(first_name) and validate_last_name(last_name) and validate_email(email) and validate_mobile(mobile_number) and check_password(password):
+        print(f'Full Name is : {first_name} {last_name}')
+        print(f'Email ID: {email}')
+        print(f'Mobile Number: {mobile_number}')
   
-
 if __name__ == "__main__":
     main()
 
